bda2312f1a9478a7de0467d009ec45398587c519
Steven Busenbarrick <sbusenba@gmail.com>
2022-11-16 11:54:22 -0800

    Move firebase logic from index.js to Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,123 @@
 import { Outlet } from "react-router-dom";
+import{useState}from 'react';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import './styles/header.css'
 function Header() {
   
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPbuSvifzyolZ6rO83Bzv1HEyRAaMhsyM",
+  authDomain: "odin-social-bb257.firebaseapp.com",
+  projectId: "odin-social-bb257",
+  storageBucket: "odin-social-bb257.appspot.com",
+  messagingSenderId: "512601198050",
+  appId: "1:512601198050:web:6cd6ae9320f6d4778bbdbb"
+};
+const [signedIn, setSignedIn] = useState(false);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+async function signIn() {
+  //Sign in Firebase with credential from the Google user.
+  var provider = new GoogleAuthProvider();
+  await signInWithPopup(auth,provider)
+  setSignedIn(true);
+}
+
+function signOutUser() {
+  // Sign out of Firebase.
+  signOut(auth);
+  setSignedIn(false);
+}
+
+
+function initFirebaseAuth() {
+  // Subscribe to the user's signed-in status
+  onAuthStateChanged(auth, authStateObserver);
+}
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  // TODO 4: Return the user's profile pic URL.
+  return auth.currentUser.photoURL || '/images/profile_placeholder.png';
+}
+
+// Returns the signed-in user's display name.
+function getUserName() {
+  // TODO 5: Return the user's display name.
+  return auth.currentUser.displayName;
+}
+
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) {
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    document.querySelector('#user-pic').style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+      document.querySelector('#user-name').textContent = userName;
+
+    // Show user's profile and sign-out button.
+    document.querySelector('#user-pic').removeAttribute('hidden');
+    document.querySelector('#user-name').removeAttribute('hidden');
+    document.querySelector('#sign-out').removeAttribute('hidden');
+
+    // Hide sign-in button.
+    document.querySelector('#sign-in').setAttribute('hidden', 'true');
+
+    // We save the Firebase Messaging Device token and enable notifications.
+    //saveMessagingDeviceToken();
+  } else {
+    // User is signed out!
+    // Hide user's profile and sign-out button.
+    document.querySelector('#user-pic').setAttribute('hidden', 'true');
+    document.querySelector('#user-name').setAttribute('hidden', 'true');
+    document.querySelector('#sign-out').setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    document.querySelector('#sign-in').removeAttribute('hidden');
+  }
+}
+function addSizeToGoogleProfilePic(url) {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+}
+
+initFirebaseAuth();
     return (
       <div className="app">
         <header className="App-header">
+            <div id ='app-logo'>Something Social</div>
+            <div id ='new-post'>+</div>
             <div id ='user-container'>
                 <div hidden id="user-pic"></div>
+                <div>
                 <div hidden id="user-name"></div>
-                <button hidden id="sign-out" className="">
+                <button hidden id="sign-out" onClick = {signOutUser}>
                     Sign-out
                 </button>
-            <button id="sign-in" className="">
+                </div>
+            <button id="sign-in" onClick={signIn}>
                 Sign-in with Google
             </button>
             </div>             
         </header>
-        <Outlet/>
+        {signedIn? <Outlet/>:null}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import RouteSwitch from './RouteSwitch';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCPbuSvifzyolZ6rO83Bzv1HEyRAaMhsyM",
-  authDomain: "odin-social-bb257.firebaseapp.com",
-  projectId: "odin-social-bb257",
-  storageBucket: "odin-social-bb257.appspot.com",
-  messagingSenderId: "512601198050",
-  appId: "1:512601198050:web:6cd6ae9320f6d4778bbdbb"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -36,8 +9,3 @@ root.render(
     <RouteSwitch />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
