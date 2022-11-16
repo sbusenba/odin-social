f53232a12f6958a99f915510ebfbc40c4f83bc47
Steven Busenbarrick <sbusenba@gmail.com>
2022-11-16 11:51:33 -0800

    Create ui for posting a new post and assoc. styles

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import "../styles/post.css"
+
+function Post (){
+    return (
+    <div id="post-form">
+        
+            <input type="file" accept="image/*"></input>
+            <input type="text"></input>
+            <button>submit</button>
+        
+    
+    
+    
+    </div>
+    )
+
+}
+
+export default Post;
