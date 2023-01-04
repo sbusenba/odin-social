50ebb05ab4a3848ac5945bc57c50cd0f6380771e
Steven Busenbarrick <sbusenba@gmail.com>
2023-01-03 17:06:59 -0800

    Update RouteSwitch due to refactoring header

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,15 +1,13 @@
 import { BrowserRouter,Routes,Route} from "react-router-dom";
 import App from "./App";
 import Post from "./components/Post"
-import Header from "./Header";
 import Feed from "./components/Feed"
 
 const RouteSwitch = () =>{
     return(
         <BrowserRouter>
             <Routes>
-                    <Route path = "/" element = {<Header/>}>
-                        <Route path="app" element = {<App/>}/>
+                    <Route path = "/" element = {<App/>}>
                         <Route path="post" element = {<Post/>}/>
                         <Route path="/" element = {<Feed/>}/>
                     </Route>        
