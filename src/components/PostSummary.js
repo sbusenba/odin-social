2c5eb3e5c0e681d21fe07a26a5b726eb5cf27e81
Steven Busenbarrick <sbusenba@gmail.com>
2023-01-12 15:48:09 -0800

    Changed div to img for image display.

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -2,16 +2,15 @@ import '../styles/postsummary.css'
 function PostSummary(props){
 
     console.log(props.post)
-    let imgStyle = {backgroundImage: `url(${props.post.imageUrl})`,
-    backgroundSize: '300px auto',
-    backgroundRepeat: 'no-repeat',
-    height: `300px`,
-    width: `auto`}
+    let imgStyle = {
+    height: `auto`,
+    width: `300px`}
 
 
     return(
         <div key={props.post.id} className="post-summary">
-            <div style={imgStyle} alt="a post"/>
+            <img src = {props.post.imageUrl} 
+            style={imgStyle} alt="a post"/>
             <p>{props.post.message}</p>
             <div className='user-block'>
                 <img className = 'poster-profile-pic'
