f13dfbef33eb95dd67adcfb922a1e1746efa39ac
Steven Busenbarrick <sbusenba@gmail.com>
2023-01-13 11:52:45 -0800

    update feed to 3 column layout, add feed.css file

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,57 @@
+import { useRef,useState,useEffect } from "react"
 import { useOutletContext } from "react-router-dom"
 import PostSummary from "./PostSummary"
+import '../styles/feed.css'
 function Feed(){
     let [,posts]= useOutletContext()
+    console.log("<feed>")
     console.log(posts)
+    let feedRef = useRef()
+    console.log(feedRef)
+    const [width, setWidth] = 
+      useState(0);
+    useEffect(() => {
+      if (feedRef.current) {
+        setWidth(feedRef.current.offsetWidth);
+        console.log(width)
+      }
+    }, []);
+    let feed1=[]
+    let feed2=[]
+    let feed3 = []
+    posts.forEach((post,index) => {     
+          let myIndex = index%3 
+          switch (myIndex) {
+            case 0:
+              feed1.push(post)
+              break;
+            case 1:
+              feed2.push(post)
+              break;
+            case 2:
+              feed3.push(post)
+              break;    
+            default:
+              break;
+          } 
+    });
     return(
-        <div className = "feed-div">
-            {posts.length>=1?posts.map((post)=><PostSummary key={post.id} post={post}/>):"No posts, post the first!"}
+        <div className="feed-container">
+            <div className = "feed-div">
+            {feed1.length>=1 ? 
+                feed1.map((post)=> <PostSummary key={post.id} post={post}/>)
+                :"No posts, post the first!"}
+            </div>
+            <div className = "feed-div">
+            {feed2.length>=1 ? 
+                feed2.map((post)=> <PostSummary key={post.id} post={post}/>)
+                :"No posts, post the first!"}
+            </div>
+            <div className = "feed-div">
+            {feed3.length>=1 ? 
+                feed3.map((post)=> <PostSummary key={post.id} post={post}/>)
+                :"No posts, post the first!"}
+            </div>
         </div>
     )
 }
