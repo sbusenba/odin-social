b3fd30b41a3c2fe998f72f5579de33c7a917210e
Steven Busenbarrick <sbusenba@gmail.com>
2023-01-18 15:56:10 -0800

    add variable to monitor entry state

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,29 @@
+import {useState} from 'react'
 import  {useOutletContext} from 'react-router-dom'
 import "../styles/post.css"
 
 function Post (){
     const [post,] = useOutletContext()
+    const [entryStatus,setEntryStatus] = useState('')
+    function fileSelected(){
+        if (entryStatus === '' ) setEntryStatus('selected');
+    }
+    function description(){
+        if (entryStatus === 'selected' ) setEntryStatus('complete');
+    }
+    function submit(){
+        post()
+        setEntryStatus('')
+        document.querySelector('#img-input').files = []
+        document.querySelector('#text-input').value = '' 
+    }
     return (
     <div id="post-form">
-        
-            <input type="file" accept="image/*" id="img-input"></input>
-            <input type="text"id="text-input"></input>
-            <button onClick={post}>submit</button>
+            <p>Choose a picture to post</p>
+            <input type="file" accept="image/*" id="img-input" onChange={fileSelected}></input>
+            <p>Description:</p>       
+            <input type="text"id="text-input" onChange = {description}></input>
+            <button onClick={submit} disabled ={(entryStatus !=='complete')}>submit</button>
         
     
     
