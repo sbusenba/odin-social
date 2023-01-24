db1dbc4f2e56609a5cb47523dfbafcdb509b6307
Steven Busenbarrick <sbusenba@gmail.com>
2023-01-24 11:20:45 -0800

    Update PostSummary to include link to PostDetail

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/postsummary.css'
 function PostSummary(props){
 
@@ -7,23 +8,26 @@ function PostSummary(props){
     height: `auto`,
     width: `300px`}
     useEffect(()=>{})
-
-
-
-    
-
+    const postLink = `post/${props.post.id}`
+    const linkStyle = {
+        margin: "0rem",
+        textDecoration: "none",
+        color: "rgb(0,0,0)",
+      };
     return(
         <div key={props.post.id} id = {props.post.id} className="post-summary">
-            <img src = {props.post.imageUrl} 
-            style={imgStyle} alt="a post"/>
-            <p>{props.post.message}</p>
+            <Link to={postLink} style={linkStyle}>
+                <img src = {props.post.imageUrl} 
+                    style={imgStyle} alt="a post"/>
+                <p>{props.post.message}</p>
+            </Link>
             <div className='user-block'>
                 <img className = 'poster-profile-pic'
                 src ={props.post.profilePicUrl} 
                 alt="user profile pic"
                 referrerPolicy="no-referrer"></img>
                 <p>{props.post.name}</p>
-            </div>
+            </div> 
         </div>
     )
 }
