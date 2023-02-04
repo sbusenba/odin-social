b1b08800aaf1227f36b71e7ae63c9e13109237df
Steven Busenbarrick <sbusenba@gmail.com>
2023-02-03 16:18:36 -0800

    Create CommentBox component.

---
+++ src/components/CommentBox.js
@@ -0,0 +1,25 @@
+import "../styles/CommentBox.css"
+function CommentBox(props){
+let imgStyle = {
+    height: `96px`,
+    width: `96px`}
+return(
+        <div className ='comment-box'> Comments:
+            {(props.comments)?props.comments.map((comment)=><div className="comment" key={comment.user+comment.comment}>
+                <div className="comment-text">
+                <p >{comment.comment}</p>
+                </div>
+                <div className="user-block">
+                    <img style ={imgStyle} src={comment.profilePicUrl}></img>
+                    <p>{comment.name}</p>                
+                </div>
+                </div>): <p>No Comments, add the first!</p>}   
+            <div className="comment-input">
+                <input type='text' className="comment-input-box"></input>
+                <button onClick={()=>props.commentFn(props.post)}>Add Comment</button>
+            </div>
+        </div>
+
+)
+}
+export default CommentBox;
